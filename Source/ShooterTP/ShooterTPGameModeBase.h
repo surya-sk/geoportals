e258a41f33a5257faf6a1ac6d6456b71c7f51e77
Surya Kashyap <surya.sk05@outlook.com>
2021-10-11 17:41:20 -0300

    Upload project files

---
+++ Source/ShooterTP/ShooterTPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShooterTPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERTP_API AShooterTPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
