e5ccde6d9cc2e4c97e12e206daedb23c8c850d37
surya-sk <surya.sk05@outlook.com>
2022-01-07 18:05:52 -0400

    Get character speed and velocity in the animinstance

--- Source/ShooterTP/ShooterAnimInstance.cpp
+++ Source/ShooterTP/ShooterAnimInstance.cpp
@@ -2,4 +2,30 @@
 
 
 #include "ShooterAnimInstance.h"
+#include "ShooterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	if (ShooterCharacter == nullptr)
+	{
+		ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+	}
+	if (ShooterCharacter)
+	{
+		// Get the lateral speed of the character from the velocity
+		FVector Velocity = ShooterCharacter->GetVelocity();
+		Velocity.Z = 0;
+		Speed = Velocity.Size();
+
+		bIsInAir = ShooterCharacter->GetCharacterMovement()->IsFalling();
+
+		bIsAccerlerating = ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f;
+	}
+}
+
+void UShooterAnimInstance::NativeInitializeAnimation()
+{
+	ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+}
 
--- Source/ShooterTP/ShooterAnimInstance.h
+++ Source/ShooterTP/ShooterAnimInstance.h
@@ -13,5 +13,25 @@ UCLASS()
 class SHOOTERTP_API UShooterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+
+	virtual void NativeInitializeAnimation() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, Meta = (AllowPrivateAccess = "true"))
+	class AShooterCharacter* ShooterCharacter;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, Meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, Meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
 	
+	/* Whether the character is moving*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, Meta = (AllowPrivateAccess = "true"))
+	bool bIsAccerlerating;
 };
