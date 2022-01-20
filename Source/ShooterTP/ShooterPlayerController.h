2dae155491d24b883fe9dde99290af48e55c7ee4
surya-sk <surya.sk05@outlook.com>
2022-01-20 19:39:47 -0400

    Create player controller class

---
+++ Source/ShooterTP/ShooterPlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ShooterPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERTP_API AShooterPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AShooterPlayerController();
+	
+};
