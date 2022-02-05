824f9519d3a52a010f93dca1a636c30ff0320c2c
surya-sk <surya.sk05@outlook.com>
2022-02-05 12:39:39 -0400

    Create defines for surface types

--- Source/ShooterTP/ShooterTP.h
+++ Source/ShooterTP/ShooterTP.h
@@ -4,3 +4,9 @@
 
 #include "CoreMinimal.h"
 
+#define EPS_Metal EPhysicalSurface::SurfaceType1
+#define EPS_Stone EPhysicalSurface::SurfaceType2
+#define EPS_Tile EPhysicalSurface::SurfaceType3
+#define EPS_Grass EPhysicalSurface::SurfaceType4
+#define EPS_Water EPhysicalSurface::SurfaceType5
+
