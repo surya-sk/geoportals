f83adbee8e8b0f1c6c616ccfabfaca9fb602939d
surya-sk <surya.sk05@outlook.com>
2022-02-12 18:30:35 -0400

    Play run animation when enemy moves

--- Source/ShooterTP/GruxAnimInstance.h
+++ Source/ShooterTP/GruxAnimInstance.h
@@ -13,5 +13,15 @@ UCLASS()
 class SHOOTERTP_API UGruxAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class AEnemy* Enemy;
 };
